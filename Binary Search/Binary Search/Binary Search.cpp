a281ce4f9dfcad4b32264946767e4a7883033482
vatansevdimeren <vatansevdimeren@gmail.com>
2026-06-23 16:59:45 +0300

    Binary and linear search released !

---
+++ Binary Search/Binary Search/Binary Search.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+int BinarySearch(int Array[], int size, int TargetValue) {
+	//Let assume the array design lower to high
+	//we return Index that is stored by the array is targettet by TargetValue
+	
+	if (Array[0] == TargetValue) {
+		return 0;
+	}
+	else if (Array[size-1] == TargetValue) {
+		return size-1;
+	}
+	int low = 0;
+	int Max = size-1	;
+	while (low < Max) {	
+		int Middle = (low + Max) / 2;
+
+		if (Array[Middle] == TargetValue) {
+			return Middle;
+		}
+		else if (Array[Middle] > TargetValue) {
+			Max = Middle - 1;
+		}
+		else {
+			low = Middle + 1;
+		}
+	}
+	return -1;
+}
+
+
+int main() {
+
+	int Array[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	
+
+	std::cout << "8 is targetted and the index : " << BinarySearch(Array, 10, 2) << "\n";
+
+	return 0;
+}
---
+++ Lineer Search/Lineer Search/Lineer Search.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int LinearSearch(int Array[], int size, int TargetValue) {
+	for (int i = 0; size > i; i++) {
+		if (Array[i] == TargetValue) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+int main() {
+	int Array[10] = { 0,1,2,3,4,5,6,7,8,9 };
+
+	std::cout << "3 hangi Index'te : " << LinearSearch(Array, sizeof(Array) / sizeof(int), 3) << std::endl;
+	std::cout << "Size of list : " << sizeof(Array) << std::endl;
+	std::cout << "Size of its paremetre : " << sizeof(int) << std::endl;
+	std::cout << "Max Element Counter size  : " << sizeof(Array) / sizeof(int);
+
+
+	return 0;
+}
