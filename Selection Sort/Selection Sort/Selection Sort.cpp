c9417628713aa625f90e3cd991962fb8ad07f4b2
vatansevdimeren <vatansevdimeren@gmail.com>
2026-06-23 18:01:26 +0300

    Selection sort has released git status!

---
+++ Selection Sort/Selection Sort/Selection Sort.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+
+void SelectionSort(int Array[], int Size) {
+	for (int i = 0; Size - 1>i; i++) {
+		int CurrentIndex = i;
+		for (int j = i; Size > j; j++) {
+			if (Array[CurrentIndex] > Array[j]) {
+				CurrentIndex = j;
+			}
+		}
+		if (CurrentIndex != i) {
+			int Temp = Array[i];
+			Array[i] = Array[CurrentIndex];
+			Array[CurrentIndex] = Temp;
+		}
+	}
+}
+void Printer(int Array[], int Size) {
+	for (int i = 0; Size > i; i++) {
+		std::cout << Array[i];
+	}
+	std::cout << "\n";
+}
+int main() {
+
+	int Array[10] = { 2,5,1,3,4,6,7,8,9,0 };
+
+	Printer(Array, sizeof(Array) / sizeof(int));
+	
+	SelectionSort(Array, sizeof(Array) / sizeof(int));
+
+	Printer(Array, sizeof(Array) / sizeof(int));
+
+
+
+
+	return 0;
+}
